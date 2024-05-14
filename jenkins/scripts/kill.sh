e36475f0d8ae0671262f3471cd7cedf0a74ae294
Mohammed Saif Fadl Alseed <saif@finehomeint.com>
2024-05-14 11:43:48 +0400

    changing the default port

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-echo 'The following command terminates the "npm start" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
+echo 'Releasing and Destroying Resources ...'
+echo 'Script in killing the Process ...'
 set -x
 kill $(cat .pidfile)
